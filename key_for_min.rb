90053f03408e4fec7829abaa313e78d4859ab184
wei chen <weichen9083@gmail.com>
2019-09-14 23:23:46 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,20 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash == {}
+    return nil 
+  else 
+    x = ""
+    y = 1000000000
+    
+    name_hash.each do |key,value|
+      if value < y 
+        y = value 
+        x = key 
+      else 
+      end 
+    end 
+    
+  end 
+  x
 end
